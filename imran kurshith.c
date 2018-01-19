6b3963e8f2c69f382ca636f3e43b29431c99de4f
imrankurshith <35135606+imrankurshith@users.noreply.github.com>
2018-01-19 16:29:46 +0530

    Create imran kurshith.c

---
+++ imran kurshith.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+   int integer1; // Declare a variable named integer1 of the type integer
+   int integer2; // Declare a variable named integer2 of the type integer
+   int sum;      // Declare a variable named sum of the type integer
+ 
+   integer1 = 55;   // Assign value to variable integer1
+   integer2 = 66;   // Assign value to variable integer1
+   sum = integer1 + integer2;   // Compute the sum
+ 
+   // Print the result
+   printf("The sum of %d and %d is %d.\n", integer1, integer2, sum);
+ 
+   return 0;
+}
